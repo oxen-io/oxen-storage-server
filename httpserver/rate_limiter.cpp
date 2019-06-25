dc6f487bd267cf55df5c46423f8560b348c581cc
Beaudan <beau@loki.network>
2019-06-25 11:32:42 +1000

    Client rate limiter plus tests

--- httpserver/rate_limiter.cpp
+++ httpserver/rate_limiter.cpp
@@ -1,6 +1,10 @@
 #include "rate_limiter.h"
 
 #include <algorithm>
+#include <assert.h>
+#include <random>
+
+#include <boost/log/trivial.hpp>
 
 constexpr uint32_t RateLimiter::BUCKET_SIZE;
 constexpr uint32_t RateLimiter::TOKEN_RATE;
@@ -51,4 +55,67 @@ bool RateLimiter::should_rate_limit(const std::string& identifier,
     }
 
     return false;
+}
+
+bool RateLimiter::should_rate_limit_client(const std::string& identifier) {
+    return should_rate_limit_client(identifier, std::chrono::steady_clock::now());
+}
+
+bool RateLimiter::should_rate_limit_client(
+    const std::string& identifier, std::chrono::steady_clock::time_point now) {
+
+    const auto it = client_buckets_.find(identifier);
+    if (it != client_buckets_.end()) {
+        auto& bucket = it->second;
+
+        fill_bucket(bucket, now);
+
+        if (bucket.num_tokens == 0) {
+            return true;
+        }
+
+        bucket.num_tokens--;
+        bucket.last_time_point = now;
+    } else {
+        if (client_ips_.size() >= MAX_CLIENTS) {
+            clean_client_buckets();
+        }
+        if (client_ips_.size() >= MAX_CLIENTS) {
+            return true;
+        }
+        const TokenBucket bucket{BUCKET_SIZE - 1, now};
+        if (client_buckets_.insert({identifier, bucket}).second) {
+            client_ips_.push_back(identifier);
+        } else {
+            BOOST_LOG_TRIVIAL(error)
+                << "Failed to insert new client rate limit bucket";
+        }
+    }
+
+    return false;
+}
+
+void RateLimiter::clean_client_buckets() {
+
+    const auto now = std::chrono::steady_clock::now();
+    auto remove_it = std::partition(
+        client_ips_.begin(), client_ips_.end(), [this, now](const std::string& ip) {
+            const auto it = client_buckets_.find(ip);
+            if (it == client_buckets_.end()) {
+                assert("Client IP missing from rate limit map" == 0);
+                return false;
+            }
+            auto& bucket = it->second;
+            fill_bucket(bucket, now);
+            return bucket.num_tokens != BUCKET_SIZE;
+        });
+
+    const auto remove_it_2 = remove_it;
+
+    while (remove_it != client_ips_.end()) {
+        client_buckets_.erase(*remove_it);
+        remove_it++;
+    }
+
+    client_ips_.erase(remove_it_2, client_ips_.end());
 }
--- unit_test/rate_limiter.cpp
+++ unit_test/rate_limiter.cpp
@@ -4,7 +4,7 @@
 
 #include <chrono>
 
-BOOST_AUTO_TEST_SUITE(request_rate_limiter)
+BOOST_AUTO_TEST_SUITE(snode_request_rate_limiter)
 
 BOOST_AUTO_TEST_CASE(it_ratelimits_only_with_empty_bucket) {
     RateLimiter rate_limiter;
@@ -51,6 +51,70 @@ BOOST_AUTO_TEST_CASE(it_handle_multiple_identifiers) {
     // other id
     BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit("otherpubkey", now),
                       false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(client_request_rate_limiter)
+
+BOOST_AUTO_TEST_CASE(it_ratelimits_only_with_empty_bucket) {
+    RateLimiter rate_limiter;
+    const std::string identifier = "myipaddress";
+    const auto now = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < RateLimiter::BUCKET_SIZE; ++i) {
+        BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(identifier, now),
+                          false);
+    }
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(identifier, now), true);
+
+    // wait just enough to allow one more request
+    const auto delta =
+        std::chrono::milliseconds(1000 / RateLimiter::TOKEN_RATE);
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(identifier, now + delta),
+                      false);
+}
+
+BOOST_AUTO_TEST_CASE(it_fills_up_bucket_steadily) {
+    RateLimiter rate_limiter;
+    const std::string identifier = "myipaddress";
+    const auto now = std::chrono::steady_clock::now();
+    // make requests at the same rate as the bucket is filling up
+    for (int i = 0; i < RateLimiter::BUCKET_SIZE * 10; ++i) {
+        const auto delta =
+            std::chrono::milliseconds(i * 1000 / RateLimiter::TOKEN_RATE);
+        BOOST_CHECK_EQUAL(
+            rate_limiter.should_rate_limit_client(identifier, now + delta), false);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(it_handles_multiple_identifiers) {
+    RateLimiter rate_limiter;
+    const std::string identifier1 = "myipaddress";
+    const auto now = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < RateLimiter::BUCKET_SIZE; ++i) {
+        BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(identifier1, now),
+                          false);
+    }
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(identifier1, now), true);
+
+    // other id
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client("otheripaddress", now),
+                      false);
+}
+
+BOOST_AUTO_TEST_CASE(it_limits_too_many_unique_clients) {
+    RateLimiter rate_limiter;
+    const auto now = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < RateLimiter::MAX_CLIENTS; ++i) {
+        BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(std::to_string(i), now),
+                          false);
+    }
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(std::to_string(RateLimiter::MAX_CLIENTS + 1), now), true);
+    // Cleanup done
+    BOOST_CHECK_EQUAL(rate_limiter.should_rate_limit_client(std::to_string(RateLimiter::MAX_CLIENTS + 1), now), false);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
