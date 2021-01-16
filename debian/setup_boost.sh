c7ebb30cc0a8060007a110bd56ecf431b4ee707c
Jason Rhinelander <jason@imaginary.ca>
2021-01-16 00:41:56 -0400

    Update bundled boost version

--- debian/setup_boost.sh
+++ debian/setup_boost.sh
@@ -8,9 +8,9 @@ set -x
 #curl -L https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2 | tar xj
 #curl -L https://deb.imaginary.stream/boost_1_72_0.tar.bz2 | tar xj
 
-curl -L https://builds.lokinet.dev/deps/boost_1_74_0.tar.bz2 | tar xj
+curl -L https://builds.lokinet.dev/deps/boost_1_75_0.tar.bz2 | tar xj
 
-cd boost_1_74_0
+cd boost_1_75_0
 
 export CC=gcc-8 CXX=g++-8
 
@@ -18,11 +18,7 @@ export CC=gcc-8 CXX=g++-8
 
 ./b2 -a --prefix=${PWD}/../boost link=static variant=release install \
         --with-program_options \
-        --with-filesystem \
         --with-system \
-        --with-thread \
-        --with-log \
-        --with-test \
         --lto
 
 
