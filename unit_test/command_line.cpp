e59cfbeac6e28c2ff8c380be72f931609f898f88
Jason Rhinelander <jason@imaginary.ca>
2021-05-25 18:05:34 -0300

    Fix --lmq-port backwards compat; test suite fixes

--- unit_test/command_line.cpp
+++ unit_test/command_line.cpp
@@ -58,7 +58,7 @@ BOOST_AUTO_TEST_CASE(it_parses_version) {
 
 BOOST_AUTO_TEST_CASE(it_parses_force_start) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--force-start"};
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--force-start"};
     BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                            const_cast<char**>(argv)));
     const auto options = parser.get_options();
@@ -67,7 +67,7 @@ BOOST_AUTO_TEST_CASE(it_parses_force_start) {
 
 BOOST_AUTO_TEST_CASE(it_parses_ip_and_port) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123"};
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123"};
     BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                            const_cast<char**>(argv)));
     const auto options = parser.get_options();
@@ -75,11 +75,23 @@ BOOST_AUTO_TEST_CASE(it_parses_ip_and_port) {
     BOOST_CHECK_EQUAL(options.port, 80);
 }
 
+BOOST_AUTO_TEST_CASE(it_parses_old_omq_port) {
+    oxen::command_line_parser parser;
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123"};
+    BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
+                                           const_cast<char**>(argv)));
+    const auto options = parser.get_options();
+    BOOST_CHECK_EQUAL(options.ip, "0.0.0.0");
+    BOOST_CHECK_EQUAL(options.port, 80);
+    BOOST_CHECK_EQUAL(options.omq_port, 123);
+}
+
+
 BOOST_AUTO_TEST_CASE(it_throw_with_invalid_port) {
     oxen::command_line_parser parser;
     const char* argv[] = {"httpserver", "0.0.0.0",
                           "8O", // notice the O instead of 0
-                          "--lmq-port", "123"};
+                          "--omq-port", "123"};
     BOOST_CHECK_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                         const_cast<char**>(argv)),
                       std::exception);
@@ -87,7 +99,7 @@ BOOST_AUTO_TEST_CASE(it_throw_with_invalid_port) {
 
 BOOST_AUTO_TEST_CASE(it_parses_oxend_rpc) {
     oxen::command_line_parser parser;
-    std::array argv = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--oxend-rpc",
+    std::array argv = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--oxend-rpc",
         "ipc:///path/to/oxend.sock"};
     BOOST_CHECK_NO_THROW(parser.parse_args(argv.size(),
                                            const_cast<char**>(argv.data())));
@@ -97,7 +109,7 @@ BOOST_AUTO_TEST_CASE(it_parses_oxend_rpc) {
 
 BOOST_AUTO_TEST_CASE(it_parses_oxend_rpc_tcp) {
     oxen::command_line_parser parser;
-    std::array argv = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--oxend-rpc",
+    std::array argv = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--oxend-rpc",
         "tcp://127.0.0.2:3456"};
     BOOST_CHECK_NO_THROW(parser.parse_args(argv.size(),
                                            const_cast<char**>(argv.data())));
@@ -107,7 +119,7 @@ BOOST_AUTO_TEST_CASE(it_parses_oxend_rpc_tcp) {
 
 BOOST_AUTO_TEST_CASE(it_parses_data_dir) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--data-dir",
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--data-dir",
                           "foobar"};
     BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                            const_cast<char**>(argv)));
@@ -117,7 +129,7 @@ BOOST_AUTO_TEST_CASE(it_parses_data_dir) {
 
 BOOST_AUTO_TEST_CASE(it_returns_default_data_dir) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123"};
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123"};
     BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                            const_cast<char**>(argv)));
     const auto options = parser.get_options();
@@ -126,7 +138,7 @@ BOOST_AUTO_TEST_CASE(it_returns_default_data_dir) {
 
 BOOST_AUTO_TEST_CASE(it_parses_log_levels) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--log-level",
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--log-level",
                           "foobar"};
     BOOST_CHECK_NO_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                            const_cast<char**>(argv)));
@@ -136,7 +148,7 @@ BOOST_AUTO_TEST_CASE(it_parses_log_levels) {
 
 BOOST_AUTO_TEST_CASE(it_throws_with_config_file_not_found) {
     oxen::command_line_parser parser;
-    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--lmq-port", "123", "--config-file",
+    const char* argv[] = {"httpserver", "0.0.0.0", "80", "--omq-port", "123", "--config-file",
                           "foobar"};
     BOOST_CHECK_THROW(parser.parse_args(sizeof(argv) / sizeof(char*),
                                         const_cast<char**>(argv)),
--- unit_test/service_node.cpp
+++ unit_test/service_node.cpp
@@ -29,12 +29,12 @@ static auto test_ip_update(ip_ports old_addr, ip_ports new_addr,
 
     auto sn = create_dummy_sn_record();
 
-    std::tie(sn.ip, sn.port, sn.lmq_port) = old_addr;
+    std::tie(sn.ip, sn.port, sn.omq_port) = old_addr;
 
     oxen::SwarmInfo si{0, std::vector<sn_record_t>{sn}};
     auto current = std::vector<oxen::SwarmInfo>{si};
 
-    std::tie(sn.ip, sn.port, sn.lmq_port) = new_addr;
+    std::tie(sn.ip, sn.port, sn.omq_port) = new_addr;
 
     oxen::SwarmInfo si2{0, std::vector<sn_record_t>{sn}};
     auto incoming = std::vector<oxen::SwarmInfo>{si2};
@@ -43,7 +43,7 @@ static auto test_ip_update(ip_ports old_addr, ip_ports new_addr,
 
     BOOST_CHECK_EQUAL(new_records[0].snodes[0].ip, std::get<0>(expected));
     BOOST_CHECK_EQUAL(new_records[0].snodes[0].port, std::get<1>(expected));
-    BOOST_CHECK_EQUAL(new_records[0].snodes[0].lmq_port, std::get<2>(expected));
+    BOOST_CHECK_EQUAL(new_records[0].snodes[0].omq_port, std::get<2>(expected));
 }
 
 BOOST_AUTO_TEST_CASE(updates_ip_address) {
@@ -72,11 +72,15 @@ BOOST_AUTO_TEST_CASE(correctly_computes_hash) {
     const auto pk = "05ffba630924aa1224bb930dde21c0d11bf004608f2812217f8ac812d6c7e3ad48";
     const auto data = "CAES1gIKA1BVVBIPL2FwaS92MS9tZXNzYWdlGrsCCAYovfqZv4YvQq8CVwutUBbhRzZw80TvR6uTYMKg9DSagrtpeEpY31L7VxawfS8aSya0SiDa4J025SkjP13YX8g5pxgQ8Z6hgfNArMqr/tSijJ9miVKVDJ63YWE85O8kyWF8tdtZR5j0Vxb+JH5U8Rg1bp7ftKk3OSf7JJMcrUUrDnctQHe540zJ2OTDJ03DfubkX5NmKqEu5nhXGxeeDv3mTiL63fjtCvZYcikfjf6Nh1AX++HTgJ9SGoEIMastGUorFrmmXb2sbjHxNiJn0Radj/VzcA9VxYwBW5+AbGQ2d9+vvm7X+8vh+jIenJfjxf+8CWER+9adNfb4YUH07I+godNCV0O0J05gzqfKdT7J8MBZzFBtKrbk8oCagPpTsq/wZyYFKFKKD+q+zh704dYBILvs5yXUA96pIAA=";
 
-    const auto hash = oxen::computeMessageHash(timestamp, ttl, pk, data);
+    const auto hash = oxen::computeMessageHash({timestamp, ttl, pk, data}, true);
 
     const auto expected = "dd5f46395dbab44c9d96711a68cd70e326c4a39d6ccce7a319b0262c18699d2044610196519ad7283e3defebcdf3bccd6499fce1254fdee661e68f0611dc3104";
 
     BOOST_CHECK_EQUAL(hash, expected);
+
+    BOOST_CHECK_EQUAL(
+            oxen::computeMessageHash({timestamp, ttl, pk, data}, false),
+            oxenmq::from_hex(expected));
 
 }
 
