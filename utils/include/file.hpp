0db1d73ba04ddd4045d5817278c0ed9b5b9ec933
Jason Rhinelander <jason@imaginary.ca>
2021-05-25 17:40:28 -0300

    oops

---
+++ utils/include/file.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <filesystem>
+#include <string>
+#include <string_view>
+
+namespace oxen {
+
+// Reads a file into a string.  Throws on error.
+std::string slurp_file(const std::filesystem::path& file);
+
+// Dumps a string to a file, overwriting if it already exists.  Throws on error.
+void dump_file(const std::filesystem::path& file, std::string_view content);
+
+}
