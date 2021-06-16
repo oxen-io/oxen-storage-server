3f1e69e79bf1e5a43af07784d0e4b1fbe1654cfb
Jason Rhinelander <jason@imaginary.ca>
2021-06-16 18:57:03 -0300

    Make unit test logger go via Catch2 UNSCOPED_INFO
    Hides log spam while still allowing Catch2 to show logs leading up to a
    failure when a test failure occurs.

--- unit_test/main.cpp
+++ unit_test/main.cpp
@@ -4,15 +4,30 @@
 #include <chrono>
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/base_sink.h>
 #include <spdlog/spdlog.h>
 
 using namespace std::literals;
 
+class catch_sink : public spdlog::sinks::base_sink<std::mutex> {
+    public:
+        catch_sink() = default;
+        ~catch_sink() override = default;
+
+    protected:
+        void sink_it_(const spdlog::details::log_msg& msg) override {
+            spdlog::memory_buf_t formatted;
+            formatter_->format(msg, formatted);
+            UNSCOPED_INFO(std::string_view(formatted.data(), formatted.size()));
+        }
+        void flush_() override {}
+};
+
 int main(int argc, char* argv[]) {
-    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
-    auto logger = std::make_shared<spdlog::logger>("oxen_logger", console_sink);
+    auto logger = std::make_shared<spdlog::logger>(
+            "oxen_logger", std::make_shared<catch_sink>());
+    logger->set_level(spdlog::level::debug);
     spdlog::register_logger(logger);
-    spdlog::flush_every(1s);
 
     return Catch::Session().run(argc, argv);
 }
