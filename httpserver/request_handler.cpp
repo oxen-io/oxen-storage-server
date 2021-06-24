4430346fb81ec8259e5e232a54f44e00eb23644c
Jason Rhinelander <jason@imaginary.ca>
2021-06-24 16:04:23 -0300

    Set content-type for post requests
    Without this requests were getting sent with
    'application/x-www-form-urlencoded' which completely broke the PN
    server.

--- httpserver/request_handler.cpp
+++ httpserver/request_handler.cpp
@@ -602,7 +602,10 @@ void RequestHandler::process_onion_req(
                 cb(std::move(res));
             },
             cpr::Url{std::move(urlstr)},
-            cpr::Header{{"User-Agent", "Oxen Storage Server/" + std::string{STORAGE_SERVER_VERSION_STRING}}},
+            cpr::Header{
+                {"User-Agent", "Oxen Storage Server/" + std::string{STORAGE_SERVER_VERSION_STRING}},
+                {"Content-Type", "application/octet-stream"}
+            },
             cpr::Timeout{ONION_URL_TIMEOUT},
             cpr::Ssl(cpr::ssl::TLSv1_2{}),
             cpr::MaxRedirects{0},
