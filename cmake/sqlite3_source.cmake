9e5f66b8f49c044c5f7052ef19a1b32d8e355fec
Jason Rhinelander <jason@imaginary.ca>
2023-12-14 18:09:28 -0400

    sqlite3 version bump
    The existing version we try to use no longer exists at the download URL.

--- cmake/sqlite3_source.cmake
+++ cmake/sqlite3_source.cmake
@@ -3,11 +3,11 @@
 # This gets used both in the full StaticBuild code *and* in the general build code when the system
 # sqlite3 version is too old.
 
-set(SQLITE3_VERSION "3430101" CACHE STRING "sqlite3 version")
+set(SQLITE3_VERSION "3440200" CACHE STRING "sqlite3 version")
 set(SQLITE3_MIRROR ${LOCAL_MIRROR} https://www.sqlite.org/2023
     CACHE STRING "sqlite3 download mirror(s)")
 set(SQLITE3_SOURCE sqlite-autoconf-${SQLITE3_VERSION}.tar.gz)
-set(SQLITE3_HASH SHA512=d459b1d8ce1036d79545bcef0194de35d3671a1477dbbef3fc94e5beb65e420cf5f3a08ce8f1837d586a16d9b59555691c1fe6351de248839b92bdef0e8d0407
+set(SQLITE3_HASH SHA512=59ad55df15eb84430f5286db2e5ceddd6ca1fc207a6343546a365c0c1baf20258e96c53d2ad48b50385608d03de09a692ae834cb78a39d1a48cb36a05722e402
     CACHE STRING "sqlite3 source hash")
 
 if(SQLITE3_VERSION MATCHES "^([0-9]+)(0([0-9])|([1-9][0-9]))(0([0-9])|([1-9][0-9]))[0-9][0-9]$")
